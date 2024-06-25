da94a331d43e0d6719bdc58ba6ff2796c65b9c08
mahavir-dash <mahavir@swnglobal.com>
2024-06-25 18:36:03 +0530

    First commit

---
+++ target/debug/deps/demo_project-93ae21840b2cdd17.d
@@ -0,0 +1,5 @@
+/Users/mahavir/Documents/SIT/demo-project/target/debug/deps/libdemo_project-93ae21840b2cdd17.rmeta: src/main.rs
+
+/Users/mahavir/Documents/SIT/demo-project/target/debug/deps/demo_project-93ae21840b2cdd17.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/demo_project-a71e1241ee7961ac.d
@@ -0,0 +1,5 @@
+/Users/mahavir/Documents/SIT/demo-project/target/debug/deps/libdemo_project-a71e1241ee7961ac.rmeta: src/main.rs
+
+/Users/mahavir/Documents/SIT/demo-project/target/debug/deps/demo_project-a71e1241ee7961ac.d: src/main.rs
+
+src/main.rs:
